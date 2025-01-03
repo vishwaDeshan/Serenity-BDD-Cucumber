782626d61bcc3862384cbd3baf3539e147396d63
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2025-01-03 08:34:59 +0530

    modify page navigation UI test

--- src/test/java/actions/NavigationSteps.java
+++ src/test/java/actions/NavigationSteps.java
@@ -1,33 +1,42 @@
 package actions;
 
+import net.serenitybdd.core.annotations.findby.FindBy;
+import net.serenitybdd.core.pages.WebElementFacade;
 import net.serenitybdd.core.steps.UIInteractionSteps;
 import net.thucydides.core.annotations.Step;
-import pageObjects.NavigationPage;
-
-import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
-import java.time.Duration;
+import org.openqa.selenium.WebDriver;
 import java.util.ArrayList;
 import java.util.List;
+import static org.junit.Assert.assertTrue;
 
 public class NavigationSteps extends UIInteractionSteps {
 
-    @Step("Navigate to the Home Page")
-    public void openHomePageWithNavigation() {
+    @FindBy(css = "div.brand.greenLogo")
+    private WebElementFacade homePageLogo;
+
+    @FindBy(css = "a.cart-header-navlink[href='#/offers']")
+    private WebElementFacade offersPageLink;
+
+    @Step("Open the Home Page")
+    public void openHomePage() {
         openUrl("https://rahulshettyacademy.com/seleniumPractise/#/");
-        withTimeoutOf(Duration.ofSeconds(10)).waitFor(visibilityOfElementLocated(NavigationPage.HOME_PAGE_LOGO));
     }
 
     @Step("Navigate to the Offers Page")
     public void navigateToOffersPage() {
-        find(NavigationPage.OFFERS_PAGE_LINK).click();
+        // Click the offers page link
+        offersPageLink.click();
+
+        // Wait for the new tab to be opened and switch to it
         List<String> tabs = new ArrayList<>(getDriver().getWindowHandles());
-        getDriver().switchTo().window(tabs.get(1));
+        getDriver().switchTo().window(tabs.get(1));  // Switch to the second tab (offers page)
     }
 
     @Step("Verify the user is on the Offers Page")
-    public boolean isOnOffersPage() {
+    public void verifyOffersPage() {
+        // Get the current URL and verify if it contains '/offers'
         String currentUrl = getDriver().getCurrentUrl();
-//        System.out.println("Current URL: " + currentUrl);
-        return currentUrl.contains("/offers");
+        assertTrue("Expected to be on the Offers page, but URL is " + currentUrl,
+                currentUrl.contains("/offers"));
     }
 }
