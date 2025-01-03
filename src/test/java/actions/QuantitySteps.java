968e9527b544ddda617d86e58322f08a0ae23e1e
Chiran Sooryakumara <85780849+Fernandophcsg@users.noreply.github.com>
2025-01-03 16:22:58 +0530

    Update QuantitySteps.java

--- src/test/java/actions/QuantitySteps.java
+++ src/test/java/actions/QuantitySteps.java
@@ -1,10 +1,21 @@
 package actions;
 
+import net.serenitybdd.core.annotations.findby.FindBy;
+import net.serenitybdd.core.pages.WebElementFacade;
 import net.serenitybdd.core.steps.UIInteractionSteps;
 import net.thucydides.core.annotations.Step;
 import pageObjects.QuantityPage;
 
 public class QuantitySteps extends UIInteractionSteps {
+
+    @FindBy(css = "a.decrement")
+    public static WebElementFacade DECREMENT_BUTTON;
+
+    @FindBy(css = "input.quantity")
+    public static WebElementFacade QUANTITY_INPUT;
+
+    @FindBy(css = "a.increment")
+    public static WebElementFacade INCREMENT_BUTTON;
 
     @Step("Navigate to the Home Page")
     public void openHomePageWithQuantity() {
@@ -13,17 +24,17 @@ public class QuantitySteps extends UIInteractionSteps {
 
     @Step("Click the increment button")
     public void clickIncrement() {
-        find(QuantityPage.INCREMENT_BUTTON).click();
+        INCREMENT_BUTTON.click();
     }
 
     @Step("Click the decrement button")
     public void clickDecrement() {
-        find(QuantityPage.DECREMENT_BUTTON).click();
+        DECREMENT_BUTTON.click();
     }
 
     @Step("Get the current quantity value")
     public int getCurrentQuantity() {
-        String quantityValue = find(QuantityPage.QUANTITY_INPUT).getAttribute("value");
+        String quantityValue = QUANTITY_INPUT.getAttribute("value");
         return Integer.parseInt(quantityValue);
     }
 }
